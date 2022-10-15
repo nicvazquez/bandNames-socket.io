92e876e0863fb77fa9687c53b7e026afb200b817
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-15 19:23:08 -0300

    Created first components

---
+++ bandnames/src/components/AddBand.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const AddBand = () => {
+	return (
+		<>
+			<h3>Add Band</h3>
+
+			<form>
+				<input type="text" className="form-control" placeholder="New band name" />
+			</form>
+		</>
+	);
+};
