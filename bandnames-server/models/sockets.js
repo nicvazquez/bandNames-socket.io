22210c79e6bd9e9fd001c348176cabbeda8589d8
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-15 20:03:56 -0300

    Increase votes

--- bandnames-server/models/sockets.js
+++ bandnames-server/models/sockets.js
@@ -13,8 +13,15 @@ class Sockets {
 		// On connection
 		this.io.on("connection", (socket) => {
 			console.log("Client connected");
+
 			// Emit all the current bands to connected client
 			socket.emit("current-bands", this.bandList.getBands());
+
+			// Vote for the band
+			socket.on("vote-band", (id) => {
+				this.bandList.increaseVotes(id);
+				this.io.emit("current-bands", this.bandList.getBands());
+			});
 		});
 	}
 }
