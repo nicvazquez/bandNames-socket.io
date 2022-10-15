4c2984f6d4ba3fc5639752cf3146f3d16f4d47a0
Nicolas <vazqueznicolasdev@gmail.com>
2022-10-15 19:56:50 -0300

    Edit band names

--- bandnames/src/components/BandList.js
+++ bandnames/src/components/BandList.js
@@ -7,6 +7,20 @@ export const BandList = ({ data }) => {
 		setBands(data);
 	}, [data]);
 
+	const changedName = (e, id) => {
+		const newName = e.target.value;
+		setBands((bands) =>
+			bands.map((band) => {
+				if (band.id === id) {
+					band.name = newName;
+				}
+				return band;
+			})
+		);
+	};
+
+	const onLostFocus = (id, name) => {};
+
 	const createRows = () => {
 		return bands.map((band) => (
 			<tr key={band.id}>
@@ -14,10 +28,15 @@ export const BandList = ({ data }) => {
 					<button className="btn btn-primary">+1</button>
 				</td>
 				<td>
-					<input value={band.name} className="form-control" />
+					<input
+						value={band.name}
+						className="form-control"
+						onChange={(e) => changedName(e, band.id)}
+						onBlur={() => onLostFocus(band.id, band.name)}
+					/>
 				</td>
 				<td>
-					<p>15</p>
+					<p>{band.votes}</p>
 				</td>
 				<td>
 					<button className="btn btn-danger">Delete</button>
